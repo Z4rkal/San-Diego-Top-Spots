4aff3ac8f1333b58550e7197ba9b7ac16069a0fd
Michael Fischer <michael@Jonathans-MacBook-Pro.local>
2019-05-30 11:53:09 -0700

    adds initial project files

--- main.js
+++ main.js
@@ -1,4 +1,9 @@
 $(document).ready(function() {
 // write your code here
-
+/*$.getJSON('data.json', data, function(data){
+    //console.log(data[1]);
+    console.log('Name: ');// + data[1].name);
+});*/
+console.log('test');
 });
+console.log('test 2');
