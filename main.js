d88b2f468602434a53b68ec70b81a8191aeea2fc
Michael Fischer <michael@Jonathans-MacBook-Pro.local>
2019-05-31 11:19:38 -0700

    Adds CSS

--- main.js
+++ main.js
@@ -8,7 +8,7 @@ $.getJSON('data.json', function(data){
         var row = $('#table-body');
         row.append('<tr class="table-row"><td class="loc-name">' + element.name + '</td>' + 
         '<td class="loc-desc">' + element.description + '</td>' + 
-        '<td class="loc-link"><a href="https://www.google.com/maps?q=' + element.location[0] + ',' + element.location[1] + '"> Open in Google Maps</a></td></tr>');
+        '<td class="loc-link"><a class="link-text" href="https://www.google.com/maps?q=' + element.location[0] + ',' + element.location[1] + '"> Open in Google Maps</a></td></tr>');
     });
 });
 //console.log('test');
